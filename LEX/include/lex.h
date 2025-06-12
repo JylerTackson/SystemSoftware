faeee81d710819c9f88bbe886105060d769124a1
Jyler <dtj1029@gmail.com>
2025-06-12 16:28:59 -0400

    Upload Assignment 2: Lexical Analyzer

---
+++ LEX/include/lex.h
@@ -0,0 +1,32 @@
+//HEADER FILE FOR THE SOURCE CODE OF THE LEXICAL ANALYSER
+//--------------------------------------------------------
+// Student Names: 
+// 	David T. Jackson - 5298477
+// 	Santiago - #######
+// Course:
+// 	COP 3402 - Systems Software
+// Instructor:
+// 	Dr. Montagne
+#ifndef LEX_H
+#define LEX_H
+
+typedef struct{
+	
+
+
+}tokenType;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ LEX/src/lex.c
@@ -0,0 +1,47 @@
+//THIS WILL BE THE SOURCE CODE FILE WE TURN IN
+//------------------------------
+//GROUP:
+//	David T. Jackson
+//	5298477
+//	Santiago H. R
+//	#PID
+//------------------------------
+//ASSIGNMENT 2:
+//	Implement a lexical analyzer for the programming lanague PL/0.
+//		- Read Source program written in PL/0
+//		- Identify Errors
+//	Output:
+//		- Source Program
+//		- Source Program Lexeme Table
+//		- Token List
+//------------------------------
+//CONSTRAINTS:
+//	Input:
+//	- Identifiers can be max of 11 chars
+//	- Numbers can be max of 5 digits
+//	- Comments should be ignored
+//	- Invisible Chars should be ignored
+//
+//	Outputs:
+//	- Token seperator can be space OR |
+//	- Identifiers must show the token and the var
+//	- In token list, numbers must show the token and value
+//		- Value must be transformed to ASCII
+//	- Consistent output, we will use spaces
+//	- Token representation will be used in the Parser
+//------------------------------
+//ERRORS:
+//	- Numbers too long
+//	- Name too long
+//	- Invalid Symbols
+//Error detected, an error message is printed and the scanner continues
+
+
+
+int main(){
+
+
+
+}
+
+
