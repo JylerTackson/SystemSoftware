f529b7884ae7e0563dbbfb8335d2667578870456
Jyler <dtj1029@gmail.com>
2025-06-14 00:24:54 -0400

    WORK ON IT SANTI

--- LEX/include/lex.h
+++ LEX/include/lex.h
@@ -10,23 +10,33 @@
 #ifndef LEX_H
 #define LEX_H
 
+
+
 typedef struct{
-	
+	char *SS = {'+','-','*','/','(',')','=',',','.','<','>',';',':'};
+	char *invis = {'\t',' ','\n'};
+}differentTokens;
 
+const char *reservedWords[] = {
+	"const", "var", "procedure", "call",
+	"begin", "end", "if", "then", "else",
+	"fi", "while", "do", "read", "write", NULL};
 
-}tokenType;
+const token_type reserved tokens[] = {
+	constsym, varsym, procsym, callsym,
+	beginsym, endsym, ifsym, thensym, elsesym,
+	fisym, whilesym, dosym, readsym, writesym};
 
+typedef enum {
+skipsym = 1, identsym, numbersym, plussym, minussym,
+multsym, slashsym, fisym, eqsym, neqsym, lessym, leqsym,
+gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+readsym , elsesym} token_type;
 
-
-
-
-
-
-
-
-
-
-
-
+int main(int argc, char *argv[]);
+void printFile(FILE *fp);
+int whitespace(int curr);
 
 #endif
--- LEX/src/lex.c
+++ LEX/src/lex.c
@@ -36,48 +36,24 @@
 //	- Invalid Symbols
 //Error detected, an error message is printed and the scanner continues
 
-<<<<<<< HEAD
 //includes
 #include "../include/lex.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+//restrictions
+#define MAX_IDENT_LENGTH 11 //max identifier length
+#define MAX_NUMBER_LENGTH 5 //max digits length
+#define MAX_LEXEMES 1000 // total token length
+
 //globals
 
-=======
-#include <stdio.h> //load needed libraries
-#include <stdlib.h> 
-#include <string.h>
->>>>>>> 936fff3310a0238c0311fe8e4dbc38f12459f057
-
-#define MAX_IDENT_LENGTH 11 //maximum letterrs in ideentifier
-#define MAX_NUMBER_LENGTH 5 //max digits in number 
-#define MAX_LEXEMES 1000 //how many total token can be stored in lexeme list
-
-typedef enum { //declaration of Token Types
-skipsym = 1, identsym, numbersym, plussym, minussym,
-multsym,  slashsym, fisym, eqsym, neqsym, lessym, leqsym,
-gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
-periodsym, becomessym, beginsym, endsym, ifsym, thensym, 
-whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
-readsym , elsesym} token_type;
-
-const char *reservedWords[] = { //program reserved words
-  "const","var","procedure","call",
-  "begin","end","if","then","else",
-  "fi","while","do","read","write",
-  NULL
-};
-
-const token_type reservedTokens[] = { //program reserved tokens
-  constsym, varsym, procsym, callsym,
-  beginsym, endsym, ifsym,  thensym, elsesym,
-  fisym,   whilesym, dosym,   readsym, writesym
-};
-
+//driver function
 int main(int argc, char *argv[]){
 
+	//Read in and Validate File
+	//--------------------------------------
 	//validate an input file was inserted
 	if(argc < 2){
 		printf("Usage: %s <input file>\n", argv[0]);
@@ -90,29 +66,45 @@ int main(int argc, char *argv[]){
 		printf("Error: Cannot open file %s\n", argv[1]);
 		return 1;
 	}
+	//--------------------------------------
 
-	//int i=0; //true value
-	//int j=1; //look ahead
-	int curr;
+
+	int curr;	//true value
+	int lookAhead; 	//lookAhead value
+
+	printFile(fp)
 
 	//main loop that will read the file and assign the token id's
 	while((curr = fgetc(fp)) != EOF){
-		
-		printf("%c", curr);
-		
-		//skip white space
-		if(1){
-		}
+		lookAhead = fgetc(fp);
 
+		//skip white space
+		if(whitespace(curr)){
+		
+		}
+		
 		//skip comments
 		if(1){
+		
 		}
 
-		
+
 	}//end while
 }//end main
 
 
 
+//print the whole file
+void printFile(FILE *fp){
+	int charptr;
+	while((charptr = fgetc(fp))!=EOF){
+		printf("%c", charptr);
+	}//end while
+}//end printFile
 
 
+
+//returns 1 if the parsed in character is white space
+int whitespace(int curr){
+	
+}
